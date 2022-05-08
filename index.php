e99c67e2527900ac35938d833cc8201d9897022f
Andre Filipe Silva Santos <andrefilipe.developer@gmail.com>
2022-05-08 03:39:52 -0300

    Servidor Apache Ativo

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <html>
 
 <head>
-<title>Meu Primeiro Site em PHP</title>
+    <title>Meu Primeiro Site em PHP</title>
 </head>
+
+<body>
+    <?php
+    for($i = 0; $i < 10;$i++){
+        print("Linha numero " . $i . "<br />");
+    }
+    ?>
+
+</body>
 
 </html>
